d3c6de812fbb14a7e6dd22af9ab826f25f391d05
Boxuan Li <liboxuan@connect.hku.hk>
2021-06-05 00:32:07 +0800

    Add experimental support for Amazon Managed KeySpace
    Closes #2370
    
    Signed-off-by: Boxuan Li <liboxuan@connect.hku.hk>

--- janusgraph-cql/src/main/java/org/janusgraph/diskstorage/cql/CQLStoreManager.java
+++ janusgraph-cql/src/main/java/org/janusgraph/diskstorage/cql/CQLStoreManager.java
@@ -131,6 +131,84 @@ public class CQLStoreManager extends DistributedStoreManager implements KeyColum
                            CQLProgrammaticConfigurationLoaderBuilder baseConfigurationLoaderBuilder) throws BackendException {
         super(configuration, DEFAULT_PORT);
         this.keyspace = determineKeyspaceName(configuration);
+        this.batchSize = configuration.get(BATCH_STATEMENT_SIZE);
+        this.atomicBatch = configuration.get(ATOMIC_BATCH_MUTATE);
+
+        this.executorService = new ThreadPoolExecutor(10,
+                100,
+                1,
+                TimeUnit.MINUTES,
+                new LinkedBlockingQueue<>(),
+                new ThreadFactoryBuilder()
+                        .setDaemon(true)
+                        .setNameFormat("CQLStoreManager[%02d]")
+                        .build());
+
+        this.session = initializeSession();
+        initializeJmxMetrics();
+        initializeKeyspace();
+
+        final Configuration global = buildGraphConfiguration()
+                .set(READ_CONSISTENCY, CONSISTENCY_QUORUM)
+                .set(WRITE_CONSISTENCY, CONSISTENCY_QUORUM)
+                .set(METRICS_PREFIX, METRICS_SYSTEM_PREFIX_DEFAULT);
+
+        final Configuration local = buildGraphConfiguration()
+                .set(READ_CONSISTENCY, CONSISTENCY_LOCAL_QUORUM)
+                .set(WRITE_CONSISTENCY, CONSISTENCY_LOCAL_QUORUM)
+                .set(METRICS_PREFIX, METRICS_SYSTEM_PREFIX_DEFAULT);
+
+        final Boolean onlyUseLocalConsistency = configuration.get(ONLY_USE_LOCAL_CONSISTENCY_FOR_SYSTEM_OPERATIONS);
+
+        final Boolean useExternalLocking = configuration.get(USE_EXTERNAL_LOCKING);
+
+        final StandardStoreFeatures.Builder fb = new StandardStoreFeatures.Builder();
+
+        fb.batchMutation(true).distributed(true);
+        fb.timestamps(true).cellTTL(true);
+        fb.keyConsistent((onlyUseLocalConsistency ? local : global), local);
+        fb.locking(useExternalLocking);
+        fb.optimisticLocking(true);
+        fb.multiQuery(false);
+
+        String partitioner = null;
+        if (configuration.has(PARTITIONER_NAME)) {
+            partitioner = getShortPartitionerName(configuration.get(PARTITIONER_NAME));
+        }
+        if (session.getMetadata().getTokenMap().isPresent()) {
+            String retrievedPartitioner = getShortPartitionerName(session.getMetadata().getTokenMap().get().getPartitionerName());
+            if (partitioner == null) {
+                partitioner = retrievedPartitioner;
+            } else if (!partitioner.equals(retrievedPartitioner)) {
+                throw new IllegalArgumentException(String.format("Provided partitioner (%s) does not match with server (%s)",
+                    partitioner, retrievedPartitioner));
+            }
+        } else if (partitioner == null) {
+            throw new IllegalArgumentException(String.format("Partitioner name not provided and cannot retrieve it from " +
+                "server, please check %s and %s options", PARTITIONER_NAME.getName(), METADATA_TOKEN_MAP_ENABLED.getName()));
+        }
+        switch (partitioner) {
+            case "DefaultPartitioner": // Amazon managed KeySpace uses com.amazonaws.cassandra.DefaultPartitioner
+                fb.timestamps(false).cellTTL(false);
+            case "RandomPartitioner":
+            case "Murmur3Partitioner": {
+                fb.keyOrdered(false).orderedScan(false).unorderedScan(true);
+                deployment = Deployment.REMOTE;
+                break;
+            }
+            case "ByteOrderedPartitioner": {
+                fb.keyOrdered(true).orderedScan(true).unorderedScan(false);
+                deployment = (hostnames.length == 1)// mark deployment as local only in case we have byte ordered partitioner and local
+                                                    // connection
+                        ? (NetworkUtil.isLocalConnection(hostnames[0])) ? Deployment.LOCAL : Deployment.REMOTE
+                        : Deployment.REMOTE;
+                break;
+            }
+            default: {
+                throw new IllegalArgumentException("Unrecognized partitioner: " + partitioner);
+            }
+        }
+        this.storeFeatures = fb.build();
         this.openStores = new ConcurrentHashMap<>();
         this.session = sessionBuilder.build(getStorageConfig(), hostnames, port, connectionTimeoutMS, baseConfigurationLoaderBuilder);
 
